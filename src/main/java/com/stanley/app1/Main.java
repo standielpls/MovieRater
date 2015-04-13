8048f72a82a20bb8165c572ac241f30ea88575b9
Stanley Chin <chin.stanlee@gmail.com>
2015-04-13 03:13:14 -0400

    added commentations and javadocs

--- src/main/java/com/stanley/app1/Main.java
+++ src/main/java/com/stanley/app1/Main.java
@@ -1,8 +1,19 @@
 package com.stanley.app1;
 
+/**
+ Description:
+ MovieRater is used to track the ratings given on IMDB of a given 'Movies' directory.
+ The result is a printed list of the movie name and the rating. The practical use of this
+ program is to view what movie has great ratings so that you may randomly pick a movie
+ to watch for your interest, effortlessly.
+
+ Author:
+ Stanley Chin
+
+ Date:
+ April 12, 2015
+ */
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-
 import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
@@ -15,22 +26,31 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Main {
-    private MovieData md;
-    private HashMap<String, Double> h;
-    private List<String> movieName;
-    private List<String> movies;
-    private HashMap<String, Integer> movie_name_year;
+    private MovieData md; //the object that carries all the information about the movie
+    private HashMap<String, Double> h;  //hash map to store the ratings based on the movie title as the key
+    private List<String> movieName; //stores all the movie names used for url detection
+    private HashMap<String, Integer> movie_name_year;   //stores the year of each movie for url detection
+    private List<String> movies;    //stores all the user-friendly movie names
 
+    /**
+     * Constructor for Main, instantiates the declared instance variables
+     */
     public Main() {
         movies = new ArrayList<String>();
         h = new HashMap<String, Double>();
         movie_name_year = new HashMap<String, Integer>();
         movieName = new ArrayList<String>();
+
+        //in addition, it loads the data, modify the titles, and parse the data
         loadData();
         modifyMovieTitle();
         parseMovieData();
     }
 
+    /**
+     * Main method used to run the program
+     * @param args
+     */
     public static void main(String[] args) {
         Main m = new Main();
         m.printData();
@@ -60,6 +80,9 @@ public class Main {
         }
     }
 
+    /**
+     * Displays the data
+     */
     public void printData() {
         System.out.println("==========================================");
         System.out.println("RATING\t\tTITLE");
@@ -70,6 +93,9 @@ public class Main {
         }
     }
 
+    /**
+     * loads the data from the directory chosen
+     */
     public void loadData() {
         JFileChooser f = new JFileChooser();
         f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
@@ -78,16 +104,19 @@ public class Main {
         //final File folder = new File("/Users/stanleychin/Desktop/Movies");
 
         listFilesForFolder(folder);
-
-
     }
 
+    /**
+     * helper method used to list the folders and stores the raw name into the movieNmae list
+     * @param folder the directory used to display the contents of
+     */
     public void listFilesForFolder(File folder) {
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
                 listFilesForFolder(fileEntry);
             } else {
                 String fileName = fileEntry.getName();
+                //picks strictly from a direct set of file names
                 if (fileName.substring(fileName.length() - 3, fileName.length()).equals("mp4") && fileName.contains("x264")
                         && !fileName.contains("ASAP") && !fileName.contains("asap")) {
                     movieName.add(fileName);
@@ -96,15 +125,19 @@ public class Main {
         }
     }
 
+    /**
+     * modifies the movie title name into something readable by the URL
+     */
     private void modifyMovieTitle() {
 
         int index = 0;
         int year = Calendar.getInstance().get(Calendar.YEAR);
         int index_to_parse = 0;
-        while (index < movieName.size()) {
-            String movieToModify = movieName.get(index);
 
-            int year_counter = 2000;
+        //Loops through the list of each movieName movie name
+        while (index < movieName.size()) {
+            String movieToModify = movieName.get(index); //the movie to modify
+            int year_counter = 2000;    //start at year 2000
 
             //get rid of the extra stuff after the year
             while (year_counter <= year) {
@@ -119,13 +152,13 @@ public class Main {
             //get rid of periods
             String parsedMovieName = movieToModify.substring(0, index_to_parse - 1);
             if (parsedMovieName.contains("&")) {
-                parsedMovieName = parsedMovieName.replaceAll("&", "%26");
+                parsedMovieName = parsedMovieName.replaceAll("&", "%26");   //replace all & with %26 (ASCII)
                 index_to_parse += 2;
                 parsedMovieName = parsedMovieName.replaceAll("\\.", " ");   //replace all periods with space
 
             }
             else if (parsedMovieName.contains(",")) {
-                parsedMovieName = parsedMovieName.replace(",", "%2C");
+                parsedMovieName = parsedMovieName.replace(",", "%2C");      //replace all commas with %2c (ASCII)
                 index_to_parse +=2;
                 parsedMovieName = parsedMovieName.replaceAll("\\.", "");   //replace all periods with space
             }
@@ -138,6 +171,9 @@ public class Main {
         }
     }
 
+    /**
+     * parses the movie data by reading the url, sending a request to the network and retrieving the data
+     */
     private void parseMovieData() {
         final String skeletonFront = "http://www.omdbapi.com/?t=";
         final String skeletonMid = "&y=";
@@ -181,6 +217,12 @@ public class Main {
             index++;
         }
     }
+
+    /**
+     * stores the data
+     * @param s name of movie
+     * @param d rating of movie
+     */
     private void storeRating(String s, Double d) {
         h.put(s, d);
         movies.add(s);
